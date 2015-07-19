b7a89c1ddc43dbb350966543d60f450b456bbfb8
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-07-19 18:18:49 +0200

    fixed fliter to False for electrons

--- Common/python/goodElectrons_cff.py
+++ Common/python/goodElectrons_cff.py
@@ -1,21 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
 electronIDs = cms.EDFilter("ElectronID",
-			   ValueMap_src = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60"),
+			   ValueMap_src = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV51"),
                            electron_src = cms.InputTag("slimmedElectrons"),
 			    #False -> runs as producer 
 			    #True -> filters events
 			    filter_flag = cms.bool(False),
 			   )
 
-looseElectrons = cms.EDFilter("CandViewSelector",
+looseElectrons = cms.EDFilter("PATElectronSelector",
                          src = cms.InputTag("electronIDs"),
-                         cut = cms.string("pt > 35")
+                         cut = cms.string("pt > 35"),
+                         filter = cms.bool(False)
                          )
 
-tightElectrons = cms.EDFilter("CandViewSelector",
+tightElectrons = cms.EDFilter("PATElectronSelector",
                          src = cms.InputTag("electronIDs"),
-                         cut = cms.string("pt > 90")
+                         cut = cms.string("pt > 90"),
+                         filter = cms.bool(False)
                          )
 
 eleSequence = cms.Sequence(electronIDs + looseElectrons + tightElectrons)
