8d336593edc4193b861b159ca722bbf42af947f4
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-10-15 17:12:19 +0200

    it's fine, just the same name in the analysis chain as you cannot add 2 sequences to the process if they are the same

--- METSystematicProducer/python/metSystematics_cff.py
+++ METSystematicProducer/python/metSystematics_cff.py
@@ -4,30 +4,27 @@ from PhysicsTools.PatAlgos.tools.helpers import cloneProcessingSnippet
 
 def CreateWLepWithMETSystematicsSequence(process):
 	process.metSequenceSystematics = cms.Sequence()
-	process.EmptySequence = cms.Sequence()
 	ListOfSystematics = ["JetResUp", "JetResDown", "JetEnUp", "JetEnDown", "MuonEnUp", "MuonEnDown", "ElectronEnUp", "ElectronEnDown", "UnclusteredEnUp", "UnclusteredEnDown"]
-	clonedMET = cms.EDProducer("METSystematicProducer", 
+	_clonedMET = cms.EDProducer("METSystematicProducer", 
                                      metSrc = cms.InputTag("slimmedMETs"),
                                      uncertaintyType = cms.string("JetResUp") )
-	WtomunuCloned = cms.EDProducer("WLeptonicProducer",
+	_WtomunuCloned = cms.EDProducer("WLeptonicProducer",
                          leptons = cms.InputTag("bestMuon"),
                          MET = cms.InputTag("slimmedMETs"),
                          cut = cms.string("pt > 200")
                          )
 
 
-	WtoenuCloned = cms.EDProducer("WLeptonicProducer",
+	_WtoenuCloned = cms.EDProducer("WLeptonicProducer",
                         leptons = cms.InputTag("bestElectron"),
                         MET = cms.InputTag("slimmedMETs"),
                         cut = cms.string("pt > 200")
                         )
 	process.shiftedMETs = cms.Sequence()
 	for iSyst in ListOfSystematics:
-		setattr(process, 'MET' + iSyst, clonedMET.clone(uncertaintyType = iSyst))
-		setattr(process, 'Wtomunu' + iSyst, WtomunuCloned.clone(MET = 'MET' + iSyst))
-		setattr(process, 'Wtoenu' + iSyst, WtoenuCloned.clone(MET = 'MET' + iSyst))
-		#setattr(process, 'SystSequence' + iSyst, getattr(process, 'MET' + iSyst) + getattr(process, 'Wtomunu' + iSyst) + getattr(process, 'Wtoenu' + iSyst))
-		#process.SystSequence = cms.Sequence(getattr(process, 'MET' + iSyst) + getattr(process, 'Wtomunu' + iSyst) + getattr(process, 'Wtoenu' + iSyst))
+		setattr(process, 'MET' + iSyst, _clonedMET.clone(uncertaintyType = iSyst))
+		setattr(process, 'Wtomunu' + iSyst, _WtomunuCloned.clone(MET = 'MET' + iSyst))
+		setattr(process, 'Wtoenu' + iSyst, _WtoenuCloned.clone(MET = 'MET' + iSyst))
 		process.metSequenceSystematics *= getattr(process, 'MET' + iSyst) * getattr(process, 'Wtomunu' + iSyst) * getattr(process, 'Wtoenu' + iSyst)
 
 	return process.metSequenceSystematics
