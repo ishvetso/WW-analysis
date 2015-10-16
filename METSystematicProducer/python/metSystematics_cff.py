229d71a94433d03401ce163438e1fbaaf5d47bdf
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-10-16 10:43:31 +0200

    remove garbage

--- METSystematicProducer/python/metSystematics_cff.py
+++ METSystematicProducer/python/metSystematics_cff.py
@@ -1,30 +1,30 @@
 import FWCore.ParameterSet.Config as cms
 
-from PhysicsTools.PatAlgos.tools.helpers import cloneProcessingSnippet
-
-def CreateWLepWithMETSystematicsSequence(process):
+def CreateWLepWithMETSystematicsSequence(process, channel):
 	process.metSequenceSystematics = cms.Sequence()
 	ListOfSystematics = ["JetResUp", "JetResDown", "JetEnUp", "JetEnDown", "MuonEnUp", "MuonEnDown", "ElectronEnUp", "ElectronEnDown", "UnclusteredEnUp", "UnclusteredEnDown"]
-	_clonedMET = cms.EDProducer("METSystematicProducer", 
+	_MET = cms.EDProducer("METSystematicProducer", 
                                      metSrc = cms.InputTag("slimmedMETs"),
-                                     uncertaintyType = cms.string("JetResUp") )
-	_WtomunuCloned = cms.EDProducer("WLeptonicProducer",
+                                     uncertaintyType = cms.string("") )
+	_Wtomunu = cms.EDProducer("WLeptonicProducer",
                          leptons = cms.InputTag("bestMuon"),
                          MET = cms.InputTag("slimmedMETs"),
                          cut = cms.string("pt > 200")
                          )
-
-
-	_WtoenuCloned = cms.EDProducer("WLeptonicProducer",
+	_Wtoenu = cms.EDProducer("WLeptonicProducer",
                         leptons = cms.InputTag("bestElectron"),
                         MET = cms.InputTag("slimmedMETs"),
                         cut = cms.string("pt > 200")
                         )
-	process.shiftedMETs = cms.Sequence()
 	for iSyst in ListOfSystematics:
-		setattr(process, 'MET' + iSyst, _clonedMET.clone(uncertaintyType = iSyst))
-		setattr(process, 'Wtomunu' + iSyst, _WtomunuCloned.clone(MET = 'MET' + iSyst))
-		setattr(process, 'Wtoenu' + iSyst, _WtoenuCloned.clone(MET = 'MET' + iSyst))
-		process.metSequenceSystematics *= getattr(process, 'MET' + iSyst) * getattr(process, 'Wtomunu' + iSyst) * getattr(process, 'Wtoenu' + iSyst)
+		setattr(process, 'MET' + iSyst, _MET.clone(uncertaintyType = iSyst))
+		if channel == "mu" : 
+			setattr(process, 'Wtomunu' + iSyst, _Wtomunu.clone(MET = cms.InputTag("MET" + iSyst)))
+			process.metSequenceSystematics *= getattr(process, 'MET' + iSyst) * getattr(process, 'Wtomunu' + iSyst) 
+		elif  channel == "el":
+			setattr(process, 'Wtoenu' + iSyst, _Wtoenu.clone(MET = cms.InputTag("MET" + iSyst)))
+			process.metSequenceSystematics *= getattr(process, 'MET' + iSyst) * getattr(process, 'Wtoenu' + iSyst)
+		else :
+			raise ValueError('Only muon and electron channel are supported, mu or el.')
 
 	return process.metSequenceSystematics
