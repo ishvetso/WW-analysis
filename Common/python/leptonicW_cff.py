d69bf6be00efaaafb059674b67298dcf81e426ae
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-10-24 16:48:00 +0200

    add resolution uncertainties

--- Common/python/leptonicW_cff.py
+++ Common/python/leptonicW_cff.py
@@ -4,16 +4,24 @@ bestMuon =cms.EDFilter("LargestPtCandViewSelector",
     src = cms.InputTag("tightMuons"), 
     maxNumber = cms.uint32(1)
   )
+bestElectron =cms.EDFilter("LargestPtCandViewSelector",
+    src = cms.InputTag("tightElectrons"), 
+    maxNumber = cms.uint32(1)
+  )
+
+# lepton energy scale uncertainties
 
 bestMuonEnUp =cms.EDProducer("LeptonSystematicsProducer",
     leptonSource = cms.InputTag("bestMuon"), 
     channel = cms.string("mu"),
+    type = cms.string("scale"),
     variation = cms.string("up")
   )
 
 bestMuonEnDown =cms.EDProducer("LeptonSystematicsProducer",
     leptonSource = cms.InputTag("bestMuon"), 
     channel = cms.string("mu"),
+    type = cms.string("scale"),
     variation = cms.string("down")
   )
 
@@ -21,18 +29,49 @@ bestMuonEnDown =cms.EDProducer("LeptonSystematicsProducer",
 bestElectronEnUp =cms.EDProducer("LeptonSystematicsProducer",
     leptonSource = cms.InputTag("bestElectron"), 
     channel = cms.string("el"),
+    type = cms.string("scale"),
     variation = cms.string("up")
   )
 
 bestElectronEnDown =cms.EDProducer("LeptonSystematicsProducer",
     leptonSource = cms.InputTag("bestElectron"), 
     channel = cms.string("el"),
-    variation = cms.string("down")
+    type = cms.string("scale"),
+    variation = cms.string("down"),
   )
 
-bestElectron =cms.EDFilter("LargestPtCandViewSelector",
-    src = cms.InputTag("tightElectrons"), 
-    maxNumber = cms.uint32(1)
+#resolution uncertainties 
+
+bestElectronResUp =cms.EDProducer("LeptonSystematicsProducer",
+    leptonSource = cms.InputTag("tightElectrons"), 
+    channel = cms.string("el"),
+    type = cms.string("resolution"),
+    variation = cms.string("up"),
+    genSource = cms.InputTag("prunedGenParticles"),
+  )
+
+bestElectronResDown =cms.EDProducer("LeptonSystematicsProducer",
+    leptonSource = cms.InputTag("tightElectrons"), 
+    channel = cms.string("el"),
+    type = cms.string("resolution"),
+    variation = cms.string("down"),
+    genSource = cms.InputTag("prunedGenParticles"),
+  )
+
+bestMuonResUp =cms.EDProducer("LeptonSystematicsProducer",
+    leptonSource = cms.InputTag("bestMuon"), 
+    channel = cms.string("mu"),
+    type = cms.string("resolution"),
+    variation = cms.string("up"),
+    genSource = cms.InputTag("prunedGenParticles"),
+  )
+
+bestMuonResDown =cms.EDProducer("LeptonSystematicsProducer",
+    leptonSource = cms.InputTag("bestMuon"), 
+    channel = cms.string("mu"),
+    type = cms.string("resolution"),
+    variation = cms.string("down"),
+    genSource = cms.InputTag("prunedGenParticles"),
   )
 
 Wtomunu = cms.EDProducer("WLeptonicProducer",
@@ -49,6 +88,6 @@ Wtoenu = cms.EDProducer("WLeptonicProducer",
                         )
 
 
-leptonicWtomunuSequence = cms.Sequence(bestMuon + bestMuonEnUp+ bestMuonEnDown + Wtomunu )
-leptonicWtoenuSequence  = cms.Sequence(bestElectron + bestElectronEnUp + bestElectronEnDown + Wtoenu)
+leptonicWtomunuSequence = cms.Sequence(bestMuon + bestMuonEnUp+ bestMuonEnDown + bestMuonResUp + bestMuonResDown + Wtomunu )
+leptonicWtoenuSequence  = cms.Sequence(bestElectron + bestElectronEnUp + bestElectronEnDown + bestElectronResUp + bestElectronResDown +  Wtoenu)
 
--- SystematicsProducers/plugins/LeptonSystematicsProducer.cc
+++ SystematicsProducers/plugins/LeptonSystematicsProducer.cc
@@ -34,7 +34,15 @@
 #include "DataFormats/PatCandidates/interface/Muon.h"     
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "DataFormats/Common/interface/RefToBaseVector.h"
-#include "DataFormats/Candidate/interface/LeafCandidate.h"     
+#include "DataFormats/Candidate/interface/LeafCandidate.h" 
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"  
+#include "DataFormats/Math/interface/deltaR.h" 
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EcalDetId/interface/EcalSubdetector.h"
+
+namespace reco {
+  typedef edm::Ptr<reco::GsfElectron> GsfElectronPtr;
+}
 
 //
 // class declaration
@@ -52,8 +60,11 @@ class LeptonSystematicsProducer : public edm::EDProducer {
       virtual void produce(edm::Event&, const edm::EventSetup&) override;
       virtual void endJob() override;
       edm::EDGetTokenT<edm::View<reco::Candidate> > leptonsToken;
+      edm::EDGetTokenT<edm::View<reco::GsfElectron> > electronsToken;
+      edm::EDGetTokenT<edm::View<reco::GenParticle> > genParticlesToken;
       std::string channel;
       std::string variation;
+      std::string type;
       
 
       // ----------member data ---------------------------
@@ -74,8 +85,13 @@ class LeptonSystematicsProducer : public edm::EDProducer {
 LeptonSystematicsProducer::LeptonSystematicsProducer(const edm::ParameterSet& iConfig):
   variation(iConfig.getParameter<std::string>("variation"))
 {
-   channel = iConfig.getParameter<std::string>("channel");  
-   leptonsToken = mayConsume<edm::View<reco::Candidate> >(iConfig.getParameter<edm::InputTag>("leptonSource"));
+   channel = iConfig.getParameter<std::string>("channel");
+   if (channel != "mu" && channel != "el") std::cerr << "Invalid channel used, use el or mu" << std::endl; 
+   type = iConfig.getParameter<std::string>("type");  
+   if (type != "scale" && channel != "resolution") std::cerr << "Invalid type of uncertainty used, use scale or resolution" << std::endl; 
+   leptonsToken = consumes<edm::View<reco::Candidate> >(iConfig.getParameter<edm::InputTag>("leptonSource"));
+   if (channel == "el") electronsToken = mayConsume<edm::View<reco::GsfElectron> >(iConfig.getParameter<edm::InputTag>("leptonSource"));
+   if (type == "resolution") genParticlesToken = mayConsume<edm::View<reco::GenParticle> >(iConfig.getParameter<edm::InputTag>("genSource"));
    produces<std::vector<reco::LeafCandidate>>();
   
   
@@ -102,34 +118,100 @@ LeptonSystematicsProducer::produce(edm::Event& iEvent, const edm::EventSetup& iS
    using namespace edm;
    
    edm::Handle<edm::View<reco::Candidate> > leptons;
+   
+ 
    iEvent.getByToken(leptonsToken, leptons);
+   
    
    std::auto_ptr<std::vector<reco::LeafCandidate>> outCollection(new std::vector<reco::LeafCandidate> );
 
    reco::LeafCandidate lepton;
    lepton = (reco::LeafCandidate)leptons->at(0);
 
+//lepton scale uncertainties
+     if (type == "scale") {
+      
+       //set the lepton uncertainty
+       double LeptonUncertainty =0.;
+       if (channel == "el") LeptonUncertainty = 0.001;//this  uncertainty is set temporary (after talking with Lindsey), not yet retrieved for 13 TeV
+       else if (channel == "mu")  {
+        LeptonUncertainty = 0.002;// 13 TeV uncertainty not yet available, use recommendation from Run I: if pt of muon < 200, uncertainty is 0.2%, else the uncertainty on the 1/pT bias is about 0.05 c/TeV : https://twiki.cern.ch/twiki/bin/view/CMS/MuonReferenceResolution#General_recommendations_for_muon
+        if (lepton.pt() > 200. ) {
+               double extraEnUncertainty = (lepton.pt()/1000.)*0.05;
+               LeptonUncertainty = sqrt(pow(LeptonUncertainty,2) + pow(extraEnUncertainty,2));
+        }
+       }
+       else std::cerr << "Invalid channel used, use el or mu" << std::endl;
 
-   
-  
-   //set the lepton uncertainty
-   double LeptonUncertainty =0.;
-   if (channel == "el") LeptonUncertainty = 0.001;//this  uncertainty is set temporary (after talking with Lindsey), not yet retrieved for 13 TeV
-   else if (channel == "mu")  {
-    LeptonUncertainty = 0.002;// 13 TeV uncertainty not yet available, use recommendation from Run I: if pt of muon < 200, uncertainty is 0.2%, else the uncertainty on the 1/pT bias is about 0.05 c/TeV : https://twiki.cern.ch/twiki/bin/view/CMS/MuonReferenceResolution#General_recommendations_for_muon
-    if (lepton.pt() > 200. ) {
-           double extraEnUncertainty = (lepton.pt()/1000.)*0.05;
-           LeptonUncertainty = sqrt(pow(LeptonUncertainty,2) + pow(extraEnUncertainty,2));
+        math::XYZTLorentzVector SmearedP4;   
+       if (variation == "up") SmearedP4.SetPxPyPzE((1 + LeptonUncertainty)*(lepton.px()),(1 + LeptonUncertainty)*(lepton.py()), lepton.pz()  ,(1 + LeptonUncertainty)*(lepton.energy()) ) ;
+       else if (variation == "down") SmearedP4.SetPxPyPzE((1 - LeptonUncertainty)*(lepton.px()),(1 - LeptonUncertainty)*(lepton.py()), lepton.pz()  ,(1 - LeptonUncertainty)*(lepton.energy()) ) ;
+       else std::cerr << "Invalid variation used in systematics, use up or down.";
+       lepton.setP4(SmearedP4);
     }
+
+// lepton resolution uncertainties
+    else if (type == "resolution") {
+      edm::Handle<edm::View<reco::GenParticle> > genParticles;
+      iEvent.getByToken(genParticlesToken, genParticles);
+      bool isEB = false;
+      double pt_gen = -1., pz_gen = -1. ;
+      double pt = lepton.pt(), pz = lepton.pz();
+      double pt_reco = lepton.pt(), pz_reco = lepton.pz();
+
+      //do  matching 
+      if (channel == "mu") {
+     for (unsigned iGen = 0; iGen < genParticles -> size(); iGen ++){
+        if ( deltaR(genParticles -> at(iGen), lepton) < 0.3 && lepton.pdgId() == genParticles -> at(iGen).pdgId() ) {
+         pt_gen = genParticles -> at(iGen).pt();
+         pz_gen = genParticles -> at(iGen).pz();
+        }  
+       }
+     }
+    // in electron channel need to define isEB, for that need to allocate reco::GsfElectron (or pat::Electron)
+    if (channel == "el") {
+      edm::Handle<edm::View<reco::GsfElectron>> electrons;
+      iEvent.getByToken(electronsToken, electrons);
+
+      auto elePtr = electrons -> ptrAt(0);
+      reco::GsfElectronPtr eleGsfPtr(elePtr);
+      const reco::CaloClusterPtr& seed = eleGsfPtr -> superCluster()->seed();
+      isEB = ( seed->seed().subdetId() == EcalBarrel );
+    }
+     
+   
+     double ElectronResUncertainty = 0.1* (isEB ? 0.02 : 0.04 );//http://arxiv.org/pdf/1502.02701v2.pdf
+     double MuonResUncertainty = 0.006; // https://twiki.cern.ch/twiki/bin/view/CMS/MuonReferenceResolution#General_recommendations_for_muon
+     if (channel == "el" && variation == "up"){
+          pt = pt + ElectronResUncertainty*pt;
+          pz = pz + ElectronResUncertainty*pz;
+      }
+     else if (channel == "el" && variation == "down"){
+          pt = pt - ElectronResUncertainty*pt;
+          pz = pz - ElectronResUncertainty*pz;
+      }
+     else if (pt_gen > 0. && channel == "mu" && variation == "up") {
+          pt = pt_gen + (1. + MuonResUncertainty)*(pt_reco - pt_gen);
+          pz = pz_gen + (1. + MuonResUncertainty)*(pz_reco - pz_gen);
+     }
+     else if (pt_gen > 0. && channel == "mu" && variation == "down") {
+          pt = pt_gen + (1. - MuonResUncertainty)*(pt_reco - pt_gen);
+          pz = pz_gen + (1. - MuonResUncertainty)*(pz_reco - pz_gen);
+     }
+     else if (pt_gen ==  -1. && pz_gen == -1. && channel == "mu") {
+      pt = pt_reco;
+      pz = pz_reco;
+    }
+     else std::cerr << "Smth is wrong, either the variation is not correct (should be up or down) or channel is not correct (should be mu or el). Please check!" << std::endl;
+     math::XYZTLorentzVector SmearedP4 = lepton.p4();
+     SmearedP4.SetPx(pt * cos(lepton.phi()));
+     SmearedP4.SetPy(pt * sin(lepton.phi()));
+     SmearedP4.SetPz(pz);
+     SmearedP4.SetE(sqrt(pt*pt + pz*pz + (lepton.mass()) * (lepton.mass()) ) );
+     lepton.setP4(SmearedP4);
+
    }
-   else std::cerr << "Invalid channel used, use el or mu" << std::endl;
-
-    math::XYZTLorentzVector SmearedP4;   
-   if (variation == "up") SmearedP4.SetPxPyPzE((1 + LeptonUncertainty)*(lepton.px()),(1 + LeptonUncertainty)*(lepton.py()), lepton.pz()  ,(1 + LeptonUncertainty)*(lepton.energy()) ) ;
-   else if (variation == "down") SmearedP4.SetPxPyPzE((1 - LeptonUncertainty)*(lepton.px()),(1 - LeptonUncertainty)*(lepton.py()), lepton.pz()  ,(1 - LeptonUncertainty)*(lepton.energy()) ) ;
-   else std::cerr << "Invalid variation used in systematics, use up or down.";
-   lepton.setP4(SmearedP4);
-
+    else std::cerr <<  "Invalid type of uncertainty used, use scale or resolution" << std::endl;
    outCollection -> push_back(lepton);
   
    iEvent.put(outCollection);
