4ed7cbcf294b1d36a2c1c2241b6d0bb71b1bdec8
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-11-03 15:40:27 +0100

    some fixes with JECs

--- TreeMaker/plugins/TreeMaker.cc
+++ TreeMaker/plugins/TreeMaker.cc
@@ -162,21 +162,16 @@ TreeMaker::TreeMaker(const edm::ParameterSet& iConfig):
   SystematicsHelper_(SystematicsHelper())
 {
   //loading JEC from text files, this is done because groomed mass should be corrected with L2L3 corrections, if this is temporary, that shouldn't be done, as we take corrections from GT
-  std::cout << "here 1" << std::endl;
-  edm::FileInPath L1("aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_MC_L1FastJet_AK8PFchs.txt");
   edm::FileInPath L2("aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_MC_L2Relative_AK8PFchs.txt");
   edm::FileInPath L3("aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_MC_L3Absolute_AK8PFchs.txt");
-  edm::FileInPath JECUncertainty("aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_MC_Uncertainty_AK8PFchs.txt");
   edm::FileInPath L2L3Res("aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_DATA_L2L3Residual_AK8PFchs.txt"); 
   std::vector<std::string> jecAK8PayloadNames_;
   if (isMC){
-    jecAK8PayloadNames_.push_back(L1.fullPath());
     jecAK8PayloadNames_.push_back(L2.fullPath());
     jecAK8PayloadNames_.push_back(L3.fullPath()); 
-    jecAK8PayloadNames_.push_back(JECUncertainty.fullPath()); 
     SystematicsHelper_  = SystematicsHelper(channel, consumesCollector());
   }
-  std::cout << "here " << std::endl;
+
   if(!isMC) jecAK8PayloadNames_.push_back(L2L3Res.fullPath()); 
 
   std::vector<JetCorrectorParameters> vPar;
@@ -465,12 +460,12 @@ TreeMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   JetCorrectorParameters const & JetCorPar = (*JetCorParCollAK8)["Uncertainty"];
   JetCorrectionUncertainty *jecUnc = new JetCorrectionUncertainty(JetCorPar);*/
   //edm::FileInPath JECUncertainty("aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_MC_Uncertainty_AK8PFchs.txt");
-
-  JetCorrectionUncertainty *jecUnc = new JetCorrectionUncertainty("/afs/cern.ch/work/i/ishvetso/aTGCRun2/CMSSW_7_4_14/src/aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_MC_Uncertainty_AK8PFchs.txt");
+  edm::FileInPath JECUncertainty("aTGCsAnalysis/TreeMaker/data/Summer15_25nsV5_MC_Uncertainty_AK8PFchs.txt");
+  JetCorrectionUncertainty *jecUnc = new JetCorrectionUncertainty(JECUncertainty.fullPath());
 
   jecUnc->setJetEta((jets -> at(0)).eta());
   jecUnc->setJetPt((jets -> at(0)).pt()); // here you must use the CORRECTED jet pt
-   
+ 
   JECunc = jecUnc->getUncertainty(true);
 
 
