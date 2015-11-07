efb796fe72489530919660291ef27a9b403c1b13
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-11-07 16:33:10 +0100

    title fixes

--- Common/test/Plotting/Plotter.cpp
+++ Common/test/Plotting/Plotter.cpp
@@ -47,13 +47,15 @@ void Plotter::Plotting(std::string OutPrefix_)
 	  c1 -> cd();
 	  TLegend *leg = new TLegend(0.8,0.7,0.98,0.93);
 	  leg ->  SetFillColor(kWhite);
-	  TH1D *hist_summed = new TH1D();
+	  TH1D *hist_summed = new TH1D(); // sum of all processes for a given variable
 	  *hist_summed = TH1D((variables.at(var_i).VarName + "summed").c_str(),( variables.at(var_i).VarName + "summed").c_str(), Nbins, variables.at(var_i).Range.low, variables.at(var_i).Range.high);
+	  hist_summed -> Sumw2();
 	    
 	//beginning of cycle over processes
 	  for (uint process_i = 0; process_i < samples.size(); process_i++)
 	  {
 	     TH1D *hist = new TH1D(((samples.at(process_i)).Processname + variables.at(var_i).VarName).c_str(),((samples.at(process_i)).Processname + variables.at(var_i).VarName).c_str(), Nbins, variables.at(var_i).Range.low, variables.at(var_i).Range.high);
+	     hist -> Sumw2();
 
 	    
 	     //beginning of cycle over files corresponding to each process
@@ -63,6 +65,7 @@ void Plotter::Plotting(std::string OutPrefix_)
 	      TFile file(((samples.at(process_i)).filenames.at(file_i)).c_str(), "READ");
 	      TTree * tree = (TTree*)file.Get("BasicTree");
 	      TH1D *temp = new TH1D((((samples.at(process_i)).Processname)+ "_temp").c_str(),((samples.at(process_i)).Processname).c_str(), Nbins,variables.at(var_i).Range.low, variables.at(var_i).Range.high);
+	      temp -> Sumw2();
 	      tree ->Project(((samples.at(process_i)).Processname + "_temp").c_str(),(variables.at(var_i).VarName).c_str(), (samples.at(process_i).selection).c_str());
 	      hist -> Add(temp);	      
 	    }
@@ -82,6 +85,7 @@ void Plotter::Plotting(std::string OutPrefix_)
 	  }
 	  //end of cycle over processes
 	  TH1D *data = new TH1D((DataSample.Processname + variables.at(var_i).VarName + "_data").c_str(),(DataSample.Processname + variables.at(var_i).VarName + "_data").c_str(), Nbins,variables.at(var_i).Range.low, variables.at(var_i).Range.high);
+	  data -> Sumw2();
 	      
 		for (uint file_i = 0; file_i < DataSample.filenames.size(); ++file_i)
 		{
@@ -91,6 +95,8 @@ void Plotter::Plotting(std::string OutPrefix_)
 		    tree ->Project(((DataSample.Processname)+ "_temp").c_str(),(variables.at(var_i).VarName).c_str(), (DataSample.selection).c_str());
 		    data -> Add(temp);
 		}
+
+		leg->AddEntry(data, "Data","pad1");
 	      
 		data->SetFillColor(78);
 		data->SetFillStyle(3004);
@@ -119,11 +125,12 @@ void Plotter::Plotting(std::string OutPrefix_)
         pad1 -> SetTopMargin(0.07);
         pad1 -> SetBottomMargin(0.);
         pad2 -> SetTopMargin(0.0);
-        pad2 -> SetBottomMargin(0.3);
+        pad2 -> SetBottomMargin(0.32);
 	    pad2 -> cd();
 	  
 
 	    TH1D *data_dif = new TH1D((variables.at(var_i).VarName + "_dif").c_str(),( variables.at(var_i).VarName + "_dif").c_str(), Nbins,variables.at(var_i).Range.low, variables.at(var_i).Range.high);
+	   // data_dif -> Sumw2();
 	    for (int iBin = 1; iBin <hist_summed -> GetNbinsX(); ++iBin)
 	    {
 			if (hist_summed -> GetBinContent(iBin) == 0.) data_dif -> SetBinContent(iBin,10000000.);
@@ -137,7 +144,7 @@ void Plotter::Plotting(std::string OutPrefix_)
 	    data_dif -> GetYaxis() -> SetLabelSize(0.15);
 	    data_dif -> GetXaxis() -> SetLabelSize(0.2);
 	    data_dif -> GetYaxis()->SetTitle("#frac{Data - MC}{MC}");
-	    data_dif -> GetXaxis()->SetTitle((variables.at(var_i)).VarName.c_str());
+	    data_dif -> GetXaxis()->SetTitle((variables.at(var_i)).Title.c_str());
 	    data_dif -> GetXaxis()->SetTitleSize(0.2);
 	    data_dif -> GetYaxis()->SetTitleOffset(0.3);
 	    data_dif -> GetYaxis()->SetTitleSize(0.2);
