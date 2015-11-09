b49708d213ef57073a049a2da08bd3a69050cef1
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-11-09 17:13:26 +0100

    remove hadronicV module

--- Common/python/hadronicW_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-hadronicV = cms.EDFilter("PATJetSelector",
-                         src = cms.InputTag("goodJets"),
-                         cut = cms.string('userFloat("ak8PFJetsCHSPrunedMass") > 40.0 & userFloat("ak8PFJetsCHSPrunedMass") < 130.0 & ((userFloat(" NjettinessAK8:tau2"))/userFloat("NjettinessAK8:tau1")) < 0.5')
-                        )
-
-hadronicVSequence = cms.Sequence(hadronicV)
