f1b59dcf6bb26850d655d05f7b39a32bc814923e
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-03-17 11:18:09 +0100

    update

--- TreeMaker/plugins/BTagHelper.h
+++ TreeMaker/plugins/BTagHelper.h
@@ -3,20 +3,20 @@
 template<class T> class BTagHelper{
 
 	BTagCalibration calib;
-	BTagCalibrationReader reader, reader_up, reader_down;
+	BTagCalibrationReader reader;
 	float MaxBJetPt = 670.;
 public:
-	BTagHelper():
-	reader(&calib, BTagEntry::OP_TIGHT,"mujets","central"),
-	reader_up(&calib, BTagEntry::OP_TIGHT,"mujets","up"),
-	reader_down(&calib, BTagEntry::OP_TIGHT,"mujets","down"){
+	BTagHelper()
+	{
 		edm::FileInPath CSVFile("aTGCsAnalysis/TreeMaker/data/CSVv2.csv");
 		calib = BTagCalibration("CSVv2", CSVFile.fullPath());
+		reader.reset(new BTagCalibrationReader(BTagEntry::OP_TIGHT, "central"));
+		reader->load(calib, BTagEntry::FLAV_B, "b");
 	}
 	double getScaleFactor(T jet){
 		float jetPt = jet.pt();
       	if (jetPt>MaxBJetPt) jetPt = MaxBJetPt;  
-		 double jet_scalefactor = reader.eval(BTagEntry::FLAV_C, jet.eta(), jetPt); 
+		 double jet_scalefactor = reader->eval(BTagEntry::FLAV_B, jet.eta(), jetPt);
 		 return jet_scalefactor;
 	}
 
