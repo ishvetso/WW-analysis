98b4d9744463a07c87bd4944ef2b7a921f5766bc
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-03-21 18:45:06 +0100

    splitting into categories

--- TreeMaker/plugins/BTagHelper.h
+++ TreeMaker/plugins/BTagHelper.h
@@ -7,7 +7,7 @@ template<class T> class BTagHelper{
 	BTagCalibration calib;
 	float MaxBJetPt;
 	TH2D *hist_eff;
-	TEfficiency *eff;
+	TEfficiency *eff_b, *eff_c, *eff_udsg;
 	double DiscrCut;
 	std::string DiscrName;
 public:
@@ -20,31 +20,42 @@ public:
 		calib = BTagCalibration("CSVv2", CSVFile.fullPath());
 		reader.reset(new BTagCalibrationReader(BTagEntry::OP_TIGHT, "central"));
 		reader->load(calib, BTagEntry::FLAV_B, "mujets");
+		reader->load(calib, BTagEntry::FLAV_C, "mujets");
+		reader->load(calib, BTagEntry::FLAV_UDSG, "incl");
 		MaxBJetPt = 670.;
 		//get measured efficiencies
 		TFile effFile(EfficiencyFile.fullPath().c_str());
-		eff = (TEfficiency*)effFile.Get("BtagAnalyzer/h2_BTaggingEff_b");
-		hist_eff = (TH2D*) eff->GetPassedHistogram();//get histogram to get binning info
+		eff_b = (TEfficiency*)effFile.Get("BtagAnalyzer/h2_BTaggingEff_b");
+		eff_c = (TEfficiency*)effFile.Get("BtagAnalyzer/h2_BTaggingEff_c");
+		eff_udsg = (TEfficiency*)effFile.Get("BtagAnalyzer/h2_BTaggingEff_udsg");
+		hist_eff = (TH2D*) eff_b->GetPassedHistogram();//get histogram to get binning info
 		DiscrCut = 0.935;
 		DiscrName = "pfCombinedInclusiveSecondaryVertexV2BJetTags";
 	}
 	double getScaleFactor(T jet){
 		float jetPt = jet.pt();
       	if (jetPt>MaxBJetPt) jetPt = MaxBJetPt;
-		 double jet_scalefactor = reader->eval(BTagEntry::FLAV_B, jet.eta(), jetPt);
-		 return jet_scalefactor;
+      	double jet_scalefactor;
+      	if (jet.partonFlavour() == 5) jet_scalefactor = reader->eval(BTagEntry::FLAV_B, jet.eta(), jetPt);
+      	else if (jet.partonFlavour() == 4) jet_scalefactor = reader->eval(BTagEntry::FLAV_C, jet.eta(), jetPt);
+      	else jet_scalefactor = reader->eval(BTagEntry::FLAV_UDSG, jet.eta(), jetPt);
+		return jet_scalefactor;
 	}
 	double getEfficiency(T jet){
 		int ptBin = hist_eff ->GetXaxis() -> FindBin(jet.pt());
 		int etaBin = hist_eff ->GetYaxis() -> FindBin(jet.eta());
 		if (jet.pt() >= hist_eff -> GetXaxis() -> GetBinUpEdge(hist_eff -> GetNbinsX()) ) ptBin =  hist_eff -> GetNbinsX();//protection in case we go above the binning limit
-		double efficiency = eff->GetEfficiency(eff->GetGlobalBin(ptBin,etaBin));
+		double efficiency;
+		if(jet.partonFlavour() == 5) efficiency = eff_b->GetEfficiency(eff_b->GetGlobalBin(ptBin,etaBin));
+		else if(jet.partonFlavour() == 4) efficiency = eff_c->GetEfficiency(eff_c->GetGlobalBin(ptBin,etaBin));
+		else  efficiency = eff_udsg->GetEfficiency(eff_udsg->GetGlobalBin(ptBin,etaBin));
 		return efficiency;
 	}
 	double getEventWeight(edm::Handle<edm::View<T>> jets){
 		double probabMC = 1., probabData = 1.;
 		for(unsigned int iBtag = 0; iBtag < jets -> size(); iBtag ++)
 		{
+			std::cout << "eta : " << jets->at(iBtag).eta() << " pt: " << jets->at(iBtag).pt() << " SF : " << getScaleFactor(jets->at(iBtag)) << std::endl;
 			if (jets->at(iBtag).bDiscriminator(DiscrName) > DiscrCut){
 				probabMC *= getEfficiency(jets->at(iBtag));
 				probabData *= getEfficiency(jets->at(iBtag)) * getScaleFactor(jets->at(iBtag));
