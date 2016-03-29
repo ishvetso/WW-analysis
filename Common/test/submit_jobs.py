5ff44ab68431530e10014173718cf4a4b06fcc66
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-03-29 13:58:39 +0200

    update submit scripts

--- Common/test/submit_jobs.py
+++ Common/test/submit_jobs.py
@@ -3,11 +3,45 @@ import sys
 import fileinput
 import optparse
 import sys
+import shutil
 
 # Author: Ivan Shvetsov
 # Numquam ponenda est pluralitas sine necessitate
 
-def createFileForJob(processName, channel, sampleName, feature, configDIR, outDir, YourJSONFile, RunRange, isMC, isSignal, wantToSubmit=False, fileName='template.txt'):
+def createConfigFile(processName, channel, isMC, isSignal):
+	if not os.path.exists("analysisConfigs"):
+		os.makedirs("analysisConfigs")
+	BTagEfficiencyPattern = "BtagEffFile = cms.string(\"\"),\n"
+	ConfigFileName = ""
+   	if isSignal and isMC :
+   		shutil.copy("../analysis_" + channel + "_signal.py", "analysisConfigs")
+   		ConfigFileName = "analysisConfigs/analysis_" + channel + "_signal.py"
+   	elif processName == "ttbar" or processName == "WZ":
+   		shutil.copy("../analysis_" + channel + "_MC.py", "analysisConfigs/analysis_" + channel + "_" + processName + ".py")
+   		ConfigFileName = "analysisConfigs/analysis_" + channel + "_" + processName + ".py"
+   		configFile = open("../analysis_" + channel + "_MC.py")
+   		outFile = open("analysisConfigs/analysis_" + channel + "_" + processName + ".py", "w+")
+   		for line in configFile:
+   			if BTagEfficiencyPattern in line :
+   				print "found"
+   				print line
+   				replaceWith = "BtagEffFile = cms.string(\"aTGCsAnalysis/TreeMaker/data/eff_" + processName + "_" + channel + ".root\"),\n"
+   				outFile.write(line.replace(BTagEfficiencyPattern, replaceWith ))
+   			else :
+   				outFile.write(line)
+   	elif isMC :
+   		shutil.copy("../analysis_" + channel + "_MC.py", "analysisConfigs")
+   		ConfigFileName = "analysisConfigs/analysis_" + channel + "_MC.py"
+   	elif not isMC :
+   		shutil.copy("../analysis_" + channel + ".py", "analysisConfigs")
+   		ConfigFileName = "analysisConfigs/analysis_" + channel + ".py"
+   	else :
+   		raise ValueError('This should not happen!')
+   	print os.path.abspath(ConfigFileName)
+   	return os.path.abspath(ConfigFileName)
+
+
+def createFileForJob(processName, channel, sampleName, feature, configFileName, outDir, YourJSONFile, RunRange, isMC, isSignal, wantToSubmit=False, fileName='template.txt'):
 	if not os.path.exists(outDir):
     		os.makedirs(outDir)
 
@@ -15,8 +49,7 @@ def createFileForJob(processName, channel, sampleName, feature, configDIR, outDi
 	patternProcessName = "$PROCESS$"
 	patternChannel = "$CHANNEL$"
 	patternSample = "$SAMPLE$"
-	patternMCvsData= "$MCvsDATA$"
-	configDIRToSearch = "$configDIR$"
+	patternConfigFile= "$CONFIGFILENAME$"
 	patternJobSplitting = "$JOBSplitting$"
 	patternForDataOnly = "$IsData$"
 	patterUnitPerJob = "$UNITSPERJOB$"
@@ -28,15 +61,8 @@ def createFileForJob(processName, channel, sampleName, feature, configDIR, outDi
 	for line in infile:
 		if patternFeature in line and patternChannel in line and patternProcessName in line:
 			tempFile.write( line.replace( patternFeature, feature).replace(patternProcessName, processName).replace(patternChannel, channel))
-		elif configDIRToSearch in line and patternChannel in line and patternMCvsData:
-			if isMC and not isSignal :
-				tempFile.write( line.replace( configDIRToSearch, configDIR).replace(patternChannel, channel).replace(patternMCvsData,"_MC"))
-			elif isMC and isSignal:
-				tempFile.write( line.replace( configDIRToSearch, configDIR).replace(patternChannel, channel).replace(patternMCvsData,"_signal"))
-			elif not isMC :
-				tempFile.write( line.replace( configDIRToSearch, configDIR).replace(patternChannel, channel).replace(patternMCvsData,""))
-			else:
-				raise ValueError('This should not happen!')
+		elif patternConfigFile in line :
+			tempFile.write( line.replace( patternConfigFile, configFileName))		
 		elif patternSample in line:
 			tempFile.write( line.replace( patternSample, sampleName))
 		elif patternJobSplitting in line:
@@ -54,7 +80,7 @@ def createFileForJob(processName, channel, sampleName, feature, configDIR, outDi
 			if isMC :
 				tempFile.write( line.replace( patterUnitPerJob, "2"))
 			else :
-				tempFile.write( line.replace( patterUnitPerJob, "20"))
+				tempFile.write( line.replace( patterUnitPerJob, "4"))
 		else:
 			tempFile.write( line)
 	tempFile.close()
@@ -66,21 +92,27 @@ def createFileForJob(processName, channel, sampleName, feature, configDIR, outDi
 
 parser =  optparse.OptionParser()
 parser.add_option('-p', '--Feature', dest="Feature", default='my_feature')
-parser.add_option('-d', '--dir', dest="directory", default='/afs/cern.ch/work/i/ishvetso/aTGCRun2/CMSSW_7_6_3/src/aTGCsAnalysis/Common/')
+parser.add_option('-d', '--dir', dest="directory", default='/afs/cern.ch/work/i/ishvetso/aTGCRun2/CMSSW_7_6_3_patch1/src/aTGCsAnalysis/Common/')
 (options, arg) = parser.parse_args()
 
 
 def submitJobs(MCBackgroundsSampleDictionary, SignalMCSampleDictionary, DataDictionaryElectronChannel, DataDictionaryMuonChannel, JSONFile, YourRunRange,wantToSubmit=False):
 	for key in MCBackgroundsSampleDictionary:
-		createFileForJob(key, "mu", MCBackgroundsSampleDictionary[key], options.Feature, options.directory,  "crabConfigs",  JSONFile, YourRunRange, True, False, wantToSubmit)
-		createFileForJob(key, "ele", MCBackgroundsSampleDictionary[key], options.Feature, options.directory,  "crabConfigs",  JSONFile, YourRunRange, True, False, wantToSubmit)
+		ConfigFileName = createConfigFile(key, "mu", True, False)
+		createFileForJob(key, "mu", MCBackgroundsSampleDictionary[key], options.Feature, ConfigFileName,  "crabConfigs",  JSONFile, YourRunRange, True, False, wantToSubmit)
+		ConfigFileName = createConfigFile(key, "ele", True, False)
+		createFileForJob(key, "ele", MCBackgroundsSampleDictionary[key], options.Feature, ConfigFileName,  "crabConfigs",  JSONFile, YourRunRange, True, False, wantToSubmit)
 	for key in SignalMCSampleDictionary:
-		createFileForJob(key, "mu", SignalMCSampleDictionary[key], options.Feature, options.directory,  "crabConfigs",  JSONFile, YourRunRange, True, True, wantToSubmit)
-		createFileForJob(key, "ele", SignalMCSampleDictionary[key], options.Feature, options.directory,  "crabConfigs",  JSONFile, YourRunRange, True, True, wantToSubmit)
+		ConfigFileName = createConfigFile(key, "mu", True, True)
+		createFileForJob(key, "mu", SignalMCSampleDictionary[key], options.Feature, ConfigFileName,  "crabConfigs",  JSONFile, YourRunRange, True, True, wantToSubmit)
+		ConfigFileName = createConfigFile(key, "ele", True, True)
+		createFileForJob(key, "ele", SignalMCSampleDictionary[key], options.Feature, ConfigFileName,  "crabConfigs",  JSONFile, YourRunRange, True, True, wantToSubmit)
 	for key in DataDictionaryElectronChannel:
-		createFileForJob(key, "ele", DataDictionaryElectronChannel[key], options.Feature, options.directory,  "crabConfigs",  JSONFile, YourRunRange, False, True, wantToSubmit)
+		ConfigFileName = createConfigFile(key, "ele", False, False)
+		createFileForJob(key, "ele", DataDictionaryElectronChannel[key], options.Feature, ConfigFileName,  "crabConfigs",  JSONFile, YourRunRange, False, True, wantToSubmit)
 	for key in DataDictionaryMuonChannel:
-		createFileForJob(key, "mu", DataDictionaryMuonChannel[key], options.Feature, options.directory,  "crabConfigs",  JSONFile, YourRunRange, False, True, wantToSubmit)
+		ConfigFileName = createConfigFile(key, "mu", False, False)
+		createFileForJob(key, "mu", DataDictionaryMuonChannel[key], options.Feature, ConfigFileName,  "crabConfigs",  JSONFile, YourRunRange, False, True, wantToSubmit)
 
 
 MCBackgroundsSampleDictionary =    {'ttbar':'/TTJets_13TeV-amcatnloFXFX-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext1-v1/MINIAODSIM',
@@ -101,11 +133,9 @@ MCBackgroundsSampleDictionary =    {'ttbar':'/TTJets_13TeV-amcatnloFXFX-pythia8/
 SignalMCSampleDictionary = {'WW-signal':'/WWToLNuQQ_aTGC_13TeV-madgraph-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM',
 							'WZ-signal': '/WZToLNu2QorQQ2L_aTGC_13TeV-madgraph-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'}
 
-DataDictionaryMuonChannel = {'data-RunC':'/SingleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD',
-							 'data-RunD':'/SingleMuon/Run2015D-16Dec2015-v1/MINIAOD'}
+DataDictionaryMuonChannel = {'data-RunD':'/SingleMuon/Run2015D-16Dec2015-v1/MINIAOD'}
 
-DataDictionaryElectronChannel = {'data-RunC':'/SingleElectron/Run2015C_25ns-16Dec2015-v1/MINIAOD',
-								 'data-RunD':'/SingleElectron/Run2015D-16Dec2015-v1/MINIAOD'}
+DataDictionaryElectronChannel = {'data-RunD':'/SingleElectron/Run2015D-16Dec2015-v1/MINIAOD'}
 
 MyJSON = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON.txt"
 	
