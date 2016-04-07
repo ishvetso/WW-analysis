e9f8c25a410cf9cc66fd50fe117ac1a0c52c5795
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-04-07 18:30:22 +0200

    remove cout

--- Common/test/Plotting/SystHelper.cpp
+++ Common/test/Plotting/SystHelper.cpp
@@ -173,7 +173,6 @@ void SystHelper::eval(Var* var, TH1D * hist_nominal){
       double errorUpQuadratic = pow(std::abs((hist_SystUp[key] -> GetBinContent(iBin)) - (hist_nominal -> GetBinContent(iBin))), 2);
       double errorDownQuadratic = pow(std::abs((hist_SystDown[key] -> GetBinContent(iBin)) - (hist_nominal -> GetBinContent(iBin))), 2);
       double errorQuadratic = std::max(errorUpQuadratic, errorDownQuadratic);
-      std::cout << sqrt(errorQuadratic)  <<  "  " << WeightNameSystematics.at(wSyst) << std::endl;
       totalErrorQuadraticErrors.at(iBin-1) += errorQuadratic;
       
     }
