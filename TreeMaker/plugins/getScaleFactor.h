6fb4cc021bd5ea09a5dff3ae27764af602c01b06
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-04-15 16:25:03 +0200

    no scale factors for muon triggers

--- TreeMaker/plugins/getScaleFactor.h
+++ TreeMaker/plugins/getScaleFactor.h
@@ -13,10 +13,6 @@ namespace sf
 	TFile fileSFMuID(SFFileMuID.fullPath().c_str());
 	TH2F *hist_mu_ID = (TH2F *) fileSFMuID.Get("tkRelIsoID_PtEtaBins_Pt20/pTtuneP_abseta_ratio");
 
-	edm::FileInPath SFFileMuTrigger("aTGCsAnalysis/TreeMaker/data/SingleMuonTrigger_Z_RunCD_Reco76X_Feb15-2.root");
-	TFile fileSFMuTrigger(SFFileMuTrigger.fullPath().c_str());
-	TH2F *hist_mu_trigger = (TH2F *) fileSFMuTrigger.Get("runD_Mu45_eta2p1_PtEtaBins/pt_abseta_ratio");
-
 	double getScaleFactor(double pt, double eta, std::string channel, std::string type){
 
 		double SF;
@@ -27,8 +23,7 @@ namespace sf
 				else SF = hist_mu_ID -> GetBinContent(hist_mu_ID -> GetXaxis() -> GetNbins(), hist_mu_ID -> GetYaxis() -> FindBin(eta));
 			}
 			else if (type == "trigger"){
-				if(hist_mu_trigger -> GetXaxis() -> FindBin(pt) != hist_mu_trigger -> GetXaxis() -> GetNbins() + 1) SF = hist_mu_trigger -> GetBinContent(hist_mu_trigger -> GetXaxis() -> FindBin(pt), hist_mu_trigger -> GetYaxis() -> FindBin(eta));
-				else SF = hist_mu_trigger -> GetBinContent(hist_mu_trigger -> GetXaxis() -> GetNbins(), hist_mu_trigger -> GetYaxis() -> FindBin(eta));
+				SF = 1.;
 			}
 			else {
 				throw cms::Exception("InvalidValue") <<  " not supported type of scale factor is used !!! " << std::endl;	
@@ -53,7 +48,7 @@ namespace sf
 		else throw cms::Exception("InvalidValue") <<  " not supported channel is used !!! " << std::endl;	
 
 		if (SF == 0.) {
-			throw cms::Exception("InvalidValue") << " pt  : " << pt << " eta : " << eta <<  ". Lepton scale factor is zero !!! " << std::endl;
+			throw cms::Exception("InvalidValue") << " pt  : " << pt << " eta : " << eta <<  ". Lepton " <<  type << " scale factor is zero !!! " << std::endl;
 		}
 		return SF;
 	}
