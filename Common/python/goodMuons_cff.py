051cde86e98d2aa96aa89e1acd47c9d6125edf98
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-04-15 16:21:47 +0200

    muon trigger and cuts changed

--- Common/python/goodMuons_cff.py
+++ Common/python/goodMuons_cff.py
@@ -7,13 +7,13 @@ HighPtMuons = cms.EDProducer("HighPtMuonSelector",
 
 looseMuons = cms.EDFilter("PATMuonSelector",
                          src = cms.InputTag("HighPtMuons"),
-                         cut = cms.string("tunePMuonBestTrack().pt > 20 & trackIso/(tunePMuonBestTrack().pt) < 0.1 & abs(tunePMuonBestTrack().eta) < 2.4"),
+                         cut = cms.string("tunePMuonBestTrack().pt > 20. & trackIso/(tunePMuonBestTrack().pt) < 0.1 & abs(tunePMuonBestTrack().eta) < 2.4"),
                          filter = cms.bool(False)
                          )
 
 tightMuons = cms.EDFilter("PATMuonSelector",
                          src = cms.InputTag("HighPtMuons"),
-                         cut = cms.string("tunePMuonBestTrack().pt > 50 & trackIso/(tunePMuonBestTrack().pt) < 0.1 & abs(tunePMuonBestTrack().eta) < 2.1"),
+                         cut = cms.string("tunePMuonBestTrack().pt > 53. & trackIso/(tunePMuonBestTrack().pt) < 0.1 & abs(tunePMuonBestTrack().eta) < 2.4"),
                          filter = cms.bool(False)
                          )
 
--- Common/python/trigger_cff.py
+++ Common/python/trigger_cff.py
@@ -1,12 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 TriggerMuon = cms.EDFilter("HLTHighLevel",
-				  HLTPaths = cms.vstring("HLT_Mu45_eta2p1_v*"),
+				  HLTPaths = cms.vstring("HLT_Mu50_v*"),
 				  throw = cms.bool(False),
 				  TriggerResultsTag = cms.InputTag("TriggerResults", "", "HLT"),
 				  andOr = cms.bool(True), eventSetupPathsKey = cms.string("")#false = and-mode (all requested triggers), true = or-mode (at least one) 
 				 )
 
+# electron trigger is disables
 TriggerElectron = cms.EDFilter("HLTHighLevel",
 				  HLTPaths = cms.vstring("HLT_Ele105_CaloIdVT_GsfTrkIdT_v*", "HLT_Ele27_WPLoose_Gsf_v*"),
 				  throw = cms.bool(False),
