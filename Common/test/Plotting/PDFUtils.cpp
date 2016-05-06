51ffec1fc6e1f7f7b34c11bede96dd0d8ec40834
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-05-06 18:50:00 +0200

    PDF4LHC recommendations implemented

--- Common/test/Plotting/PDFUtils.cpp
+++ Common/test/Plotting/PDFUtils.cpp
@@ -1,8 +1,12 @@
+#ifndef  PDFUTILS
+#define PDFUTILS
 /*
 * create an envelope for the list of histogram
 *
 */
 #include <algorithm>
+#include "TH1D.h"
+#include <iostream>
 
 std::string random_string( size_t length )
 {
@@ -44,3 +48,32 @@ TH1D * makeEnvelope(std::vector<TH1D*> hists, std::string variation){
 
 	return hist;
 }
+
+TH1D * makePDF4LHC(std::vector<TH1D*> hists, std::string variation){
+	if (hists.size()!= 100)	throw std::runtime_error("you should have exactly 100 variations according to PDF4LHC recommendations");
+		
+	int Nbins = hists[0]->GetNbinsX();
+	std::string name = random_string(10);
+	TH1D *hist = new TH1D( (name + variation).c_str(),(name + variation).c_str(), Nbins,hists[0]->GetXaxis()->GetBinLowEdge(1),hists[0]->GetXaxis()->GetBinUpEdge(hists[0]->GetNbinsX()));
+
+	for (int iBin = 1; iBin <= Nbins; iBin++ )
+	{
+		std::vector<double> ValuesPerBin;
+		for (unsigned int iHist = 0; iHist < hists.size(); iHist ++)
+		{
+			ValuesPerBin.push_back(hists.at(iHist)->GetBinContent(iBin));
+		}
+
+		std::sort(ValuesPerBin.begin(), ValuesPerBin.end());		
+		
+		if(variation == "up")hist -> SetBinContent(iBin,ValuesPerBin.at(83));
+		else if (variation == "down")hist -> SetBinContent(iBin,ValuesPerBin.at(15));
+		else {
+			std::cerr << "Wrong type of variation, please use up or down" << std::endl;
+		}
+		ValuesPerBin.clear();
+	}
+
+	return hist;
+}
+#endif
