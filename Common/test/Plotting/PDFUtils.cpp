29f4728feb81502fb8c171e6f8206d054f9365ea
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-05-13 14:35:56 +0200

    for debugging

--- Common/test/Plotting/PDFUtils.cpp
+++ Common/test/Plotting/PDFUtils.cpp
@@ -28,6 +28,7 @@ TH1D * makeEnvelope(std::vector<TH1D*> hists, std::string variation){
 	int Nbins = hists[0]->GetNbinsX();
 	std::string name = random_string(10);
 	TH1D *hist = new TH1D( (name + variation).c_str(),(name + variation).c_str(), Nbins,hists[0]->GetXaxis()->GetBinLowEdge(1),hists[0]->GetXaxis()->GetBinUpEdge(hists[0]->GetNbinsX()));
+	hist -> Sumw2();
 
 	for (int iBin = 1; iBin <= Nbins; iBin++ )
 	{
@@ -37,6 +38,7 @@ TH1D * makeEnvelope(std::vector<TH1D*> hists, std::string variation){
 			ValuesPerBin.push_back(hists.at(iHist)->GetBinContent(iBin));
 		}
 		double MaxValue = *std::max_element(ValuesPerBin.begin(), ValuesPerBin.end());
+		std::cout << MaxValue << "   " <<  (std::max_element(ValuesPerBin.begin(), ValuesPerBin.end())) - ValuesPerBin.begin() << std::endl;
 		double MinValue = *std::min_element(ValuesPerBin.begin(), ValuesPerBin.end());
 		if(variation == "up")hist -> SetBinContent(iBin,MaxValue);
 		else if (variation == "down")hist -> SetBinContent(iBin,MinValue);
@@ -55,6 +57,7 @@ TH1D * makePDF4LHC(std::vector<TH1D*> hists, std::string variation){
 	int Nbins = hists[0]->GetNbinsX();
 	std::string name = random_string(10);
 	TH1D *hist = new TH1D( (name + variation).c_str(),(name + variation).c_str(), Nbins,hists[0]->GetXaxis()->GetBinLowEdge(1),hists[0]->GetXaxis()->GetBinUpEdge(hists[0]->GetNbinsX()));
+	hist -> Sumw2();
 
 	for (int iBin = 1; iBin <= Nbins; iBin++ )
 	{
