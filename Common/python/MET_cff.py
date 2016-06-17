739fb5153963a2460bb7a26f0652bea13d205695
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-06-17 15:27:19 +0200

    MET cuts relaxed

--- Common/python/MET_cff.py
+++ Common/python/MET_cff.py
@@ -2,13 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 METele = cms.EDFilter("PATMETSelector",
                          src = cms.InputTag("slimmedMETs"),
-                         cut = cms.string("pt > 80"),
+                         cut = cms.string("pt > 40"),
                          filter = cms.bool(True)
                          )
 
 METmu = cms.EDFilter("PATMETSelector",
                          src = cms.InputTag("slimmedMETs"),
-                         cut = cms.string("pt > 40"),
+                         cut = cms.string("pt > 35"),
                          filter = cms.bool(True)
                          )
 
